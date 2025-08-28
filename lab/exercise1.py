8b47e21a1f6656fb1e554fca90deba296c478677
TrollGuy87 <115047191+TrollGuy87@users.noreply.github.com>
2025-08-28 13:23:23 -0400

    exercise for cs1300

---
+++ lab/exercise1.py
@@ -0,0 +1,47 @@
+# exercise1.py
+# Variable Type Demonstration
+# Your Name
+# Date
+# TODO: Create variables of each type
+# Integer variable for your age
+
+age = 19
+
+# Float variable for your GPA (make one up if needed)
+
+gpa = 3.8
+# String variable for your name
+
+name = "Avery Layos"
+
+# Boolean variable for whether you like Python
+likes_python = True
+
+# None variable for something not yet defined
+
+future_job = "Cybersecurity"
+# TODO: Print each variable and its type
+
+print("Age:", age, "- Type:", type(age))
+print("GPA:", gpa, "- Type:", type(gpa))
+print("Name:", name, "- Type:", type(name))
+print("Likes python:", likes_python, "- Type:", type(likes_python))
+print("Future:", future_job, "- Type:", type(future_job))
+
+# Continue for all variables...
+# TODO: Try these operations and observe what happens
+# 1. Add 1 to age and print result
+age += 1
+print("Age:", age)
+
+# 2. Add 0.1 to GPA and print result
+gpa += 0.1
+print("GPA:", gpa)
+
+# 3. Multiply your name by 3 and print result
+name *= 3
+print("Name:", name)
+
+# 4. What happens if you try: age + name? (Try it and comment out if it errors)
+# name + age
+# print("Name:", name, " Age: ", age)"
